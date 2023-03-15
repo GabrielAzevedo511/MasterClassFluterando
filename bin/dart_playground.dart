b9180d6b90c4291e7c4a6f1b771a6271be376b0e
Gabriel Robinson de Azevedo <19100080@liberato.com.br>
2023-03-15 10:14:59 -0300

    finish ex05

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -4,9 +4,10 @@ import 'dart:io';
 
 import 'ex03.dart';
 import 'ex04/ex04.dart';
+import 'ex05.dart';
 
 void main(List<String> arguments) {
-  exercicio4();
+  exercicio5();
   // print(
   //     'Digite 0 para ver o exercicio IMC. \nDigite 1 para ver o exercicio fibonacci. \nDigite 2 para ver o exercicio regra de 3.');
   // String? dataFromUser = stdin.readLineSync(retainNewlines: false);
